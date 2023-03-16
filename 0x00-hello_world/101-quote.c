686c33095350a4c70c874a4574952fc81f36aa29
Blxee <aminedev57@gmail.com>
2023-03-16 13:07:38 +0000

    add: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print phrase without using printf or puts
+ * Return: 0
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, sizeof(char) * sizeof(quote));
+	return (0);
+}
