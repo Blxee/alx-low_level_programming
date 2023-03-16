47dbc197bacd2cab73f175a3fbbe8d67d598e9b5
Blxee <aminedev57@gmail.com>
2023-03-16 13:34:53 +0000

    fix: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n";
+
 	write(2, quote, sizeof(char) * sizeof(quote) - 1);
-	return (0);
+	return (1);
 }
