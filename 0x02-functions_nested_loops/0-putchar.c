5c50458c024cebe9eed807e24092fd98f52ad3c7
Blxee <aminedev57@gmail.com>
2023-03-21 11:19:52 +0000

    fix 0-putchar.c task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include <_putchar.h>
+#include "main.h"
 
 /**
  * main - Entry point
  * Description: prints _putchar
  * Return: 0 (success)
  */
-int main(void)
+void putchar()
 {
-	printf("_putchar\n");
+	char str[] = "_putchar\n\0";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
 	return (0);
 }
