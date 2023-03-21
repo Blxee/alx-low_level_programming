b2bd0ef87aab2cc688acf3a0a33a70c0ed366b40
Blxee <aminedev57@gmail.com>
2023-03-21 11:43:42 +0000

    add 2-print_alphabet_10x.c task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - print the alphabet in lowercase 10x times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		while (c != 'z' - 1)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+		c = 'a';
+	}
+}
