c1be2c5a2a9deb8f1bff0ab70fa1f82974f62f6a
Blxee <aminedev57@gmail.com>
2023-03-21 11:47:13 +0000

    fix 2-print_alphabet_10x.c task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet_x10 - print the alphabet in lowercase 10x times
- * Description: prints the alphabet 10x times
+ * print_alphabet_x10 - print the alphabet in lowercase 10x times
  */
 void print_alphabet_x10(void)
 {
