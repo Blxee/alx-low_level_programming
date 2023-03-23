2b0dab7f9eaeb20e365b6eea9dc5f4029a62f63f
Blxee <aminedev57@gmail.com>
2023-03-23 18:02:17 +0000

    fix: 101-print_number.c task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
 
 void print_number(int n)
 {
-	int max = 1000000000, printing = 0, digit;
+	int printing = 0, digit;
 
 	if (n == 0)
 		_putchar('0');
@@ -19,7 +19,7 @@ void print_number(int n)
 
 	while (max)
 	{
-		digit = n / max % 10;
+		digit = n / 1000000000 % 10;
 		if (digit > 0)
 			printing = 1;
 		if (printing)
