36ca9bb771942b2e2f9219db98a1000f19db4475
Blxee <aminedev57@gmail.com>
2023-03-23 10:53:29 +0000

    add: 2-mul.c task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first operand
+ * @b: The second operand
+ * Return: The product of the operands
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+	
+	return (product);
+}
