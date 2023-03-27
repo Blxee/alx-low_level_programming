b97bd414c0f42bb3ddbbc272076d4dbb7f8d3e02
Blxee <aminedev57@gmail.com>
2023-03-27 20:18:35 +0000

    add: optional task number 0

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts from string to integer
+ * @s: the string to convert
+ * Return: 0 if the conversion is successful, 1 elsewise
+ */
+
+int _atoi(char *s)
+{
+	int started = 0,
+			sign = 1,
+			result = 0;
+
+	while (*s != '\0')
+	{
+
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			result *= 10;
+			result += *s - '0';
+			started = 1;
+		}
+		else
+		{
+			if (started)
+				break;
+			sign = 1;
+		}
+
+		s++;
+	}
+
+	return (result * sign);
+}
