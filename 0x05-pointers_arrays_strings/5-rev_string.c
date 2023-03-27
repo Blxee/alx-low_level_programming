831cac9b487f279142803c065774a531cbf278dd
Blxee <aminedev57@gmail.com>
2023-03-27 15:26:48 +0000

    fix: 5-rev_string.c task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,3 +12,19 @@ void rev_string(char *s)
 	for (i = 0, len = _strlen(s); i < len; i++)
 		s[i] = s[len - i - 1];
 }
+
+/**
+ * _strlen - Determine the length of a string
+ * @s: the address of the first character of a string
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
