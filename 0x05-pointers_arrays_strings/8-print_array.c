ffb808992328523226b57ca131d16be9764e3a92
Blxee <aminedev57@gmail.com>
2023-03-27 15:49:38 +0000

    add: 8-print_array.c task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Prints n amount of an integer array followed by a new line
+ * @a: the address of the first element of an integer array
+ * @n: the amount to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf(i == n - 1 ? "%d\n" : "%d, ", a[i]);
+	}
+}
