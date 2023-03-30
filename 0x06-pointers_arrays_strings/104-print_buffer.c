e57bcd995ff27445208bb5cb128975a49ef1bc3f
Blxee <aminedev57@gmail.com>
2023-03-30 13:50:47 +0000

    fix: 104-print_buffer.c task

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,7 +17,7 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i < size; i += 0xa, b += 0xa)
 	{
-		printf("%08x: ", 255);
+		printf("%08x: ", i);
 
 		for (j = 0; j < 0xa; j += 2)
 		{
@@ -40,4 +40,15 @@ void print_buffer(char *b, int size)
 		}
 		printf("\n");
 	}
+}
+
+
+int main(void)
+{
+	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+
+	printf("%s\n", buffer);
+	printf("---------------------------------\n");
+	print_buffer(buffer, sizeof(buffer));
+	return (0);
 }
