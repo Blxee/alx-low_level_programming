0f08fb6716a40641174748451c6f86f94e217bbe
Blxee <aminedev57@gmail.com>
2023-03-30 11:51:11 +0000

    fix: 101-print_number.c task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		num = -num;
 	}
 
-	if (n < 10)
-	{
-		_putchar('0' + n);
-		return;
-	}
+	if (num / 10)
+		print_number(num / 10);
 
-	print_number(n / 10);
-	_putchar('0' + n % 10);
+	_putchar(48 + num % 10);
 }
