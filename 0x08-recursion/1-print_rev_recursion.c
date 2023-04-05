3f144e1b7f14c1e207f43ee594ef0d1f71a2de45
Blxee <aminedev57@gmail.com>
2023-04-05 05:18:46 +0000

    add: 1-print_rev_recursion.c task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse to stdout
+ * @s: the string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(++s);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
