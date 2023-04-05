32137c6fad0247ce031651b78bc8b50cce38fef4
Blxee <aminedev57@gmail.com>
2023-04-05 06:47:11 +0000

    add: 5-sqrt_recursion.c task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculates the square root of an integer
+ * @n: an integer number
+ * Return:
+ *	the square root of @n
+ *	-1 if the number has no square root or it is float number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_inner_sqrt(n, n / 2 + 1));
+}
+
+/**
+ * _inner_sqrt - helper recursive function for _sqrt_recursion
+ * @n: an integer number
+ * @m: a helper value meant to be used by _sqrt_recursion
+ * Return:
+ *	the square root of @n
+ *	-1 if the number has no square root or it is float number
+ */
+
+int _inner_sqrt(int n, int m)
+{
+	if (m * m == n)
+		return (m);
+	if (m == 0)
+		return (-1);
+	return (_inner_sqrt(n, m - 1));
+}
