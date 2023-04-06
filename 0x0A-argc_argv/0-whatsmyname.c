6f1e8d17ccb45351adf9c1d65cdc706a65070679
Blxee <aminedev57@gmail.com>
2023-04-06 05:48:44 +0000

    add: 0-whatsmyname.c task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints its executable file name
+ * @argc: number of command line arguments
+ * @argv: the command line arguments
+ * Return: Always EXIT_SUCCESS = 0
+ */
+
+int main(int argc, char *argv[])
+{
+	puts(argv[0]);
+	return (EXIT_SUCCESS);
+}
