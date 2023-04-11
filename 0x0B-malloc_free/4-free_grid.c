cde167bf5101b19ef7f284e77dac1eedc50abd7b
Blxee <aminedev57@gmail.com>
2023-04-11 15:29:52 +0000

    fix: 4-free_grid.c task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,9 +8,7 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
-
-	for (i = 0; i < height; i++)
-		free(grid[i]);
+	for (height--; height >= 0; height--)
+		free(grid[height]);
 	free(grid);
 }
