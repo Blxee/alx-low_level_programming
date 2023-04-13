f846a99cc7c39f05e5ea635ada1f0ff519e1cca7
Blxee <aminedev57@gmail.com>
2023-04-13 05:18:30 +0000

    add: 0-malloc_checked.c task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory in the heap
+ * @b: amount of memory to allocate (in bytes)
+ * Returns: A pointer to the alllcated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+  void *p = malloc(b);
+
+  return (p);
+}
