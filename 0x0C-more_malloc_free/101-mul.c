06bce3840677c9117fe36baab0de8587d91a4a43
Blxee <aminedev57@gmail.com>
2023-04-14 01:52:12 +0000

    fix: 101-mul.c task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,9 +19,15 @@ int main(int argc, char *argv[])
 	char *result, *num1 = argv[1], *num2 = argv[2];
 
 	if (*num1 == '-')
+	{
 		neg = !neg;
+		num1++;
+	}
 	if (*num2 == '-')
+	{
 		neg = !neg;
+		num2++;
+	}
 	if (argc != 3 || !*num1 || !*num2)
 		goto exit_label;
 	for (i = 0; num1[i]; i++)
