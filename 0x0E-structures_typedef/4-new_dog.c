6d7e97ff52ecdfe2ea6349fbc19cd8504b8bcb7a
Blxee <aminedev57@gmail.com>
2023-04-17 05:34:07 +0000

    Add: 4-new_dog.c task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog struct from fields
+ *
+ * @name: the dog' name
+ * @age: the dog's age
+ * @owner: the name of the dog's owner
+ *
+ * Return: a new dog struct from fields
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *name_cp, *owner_cp;
+
+	new = malloc(sizeof(dog_t));
+	name_cp = malloc(strlen(name) * sizeof(char) + 1);
+	owner_cp = malloc(strlen(owner) * sizeof(char) + 1);
+
+	if (!new || !name_cp || !owner_cp)
+		return (NULL);
+
+	strcpy(name_cp, name);
+	strcpy(owner_cp, owner);
+	*new = (dog_t){name_cp, age, owner_cp};
+
+	return (new);
+}
