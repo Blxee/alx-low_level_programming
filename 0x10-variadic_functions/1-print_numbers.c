fd5eb34ada02578a938fcc47e091d859ff1b6eb3
Blxee <aminedev57@gmail.com>
2023-04-20 04:59:28 +0000

    Add: 1-print_numbers.c task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints variable amount of numbers to stdout
+ *
+ * @separator: a separator string to use between numbers
+ * @n: argument count
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  unsigned int i;
+  va_list va;
+
+  if (!separator)
+    separator = "";
+
+  va_start(va, n);
+  for (i = 0; i < n; i++)
+  {
+    if (i == n - 1)
+      printf("%d\n", va_arg(va, int));
+    else
+      printf("%d%s", va_arg(va, int), separator);
+  }
+  va_end(va);
+}
