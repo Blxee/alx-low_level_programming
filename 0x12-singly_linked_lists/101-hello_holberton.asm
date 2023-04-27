c6d688a8001ec0c7483c5817f35fd6d792530b6d
Blxee <aminedev57@gmail.com>
2023-04-27 15:09:14 +0100

    Fixed: 101-hello_hoberton.asm task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,42 +1,18 @@
-%line 1+1 temp.s
- .text
- .file "temp.c"
- .globl main // -- Begin function main
- .p2align 2
- .type main,@function
-main: // @main
- .cfi_startproc
-// %bb.0:
- sub sp, sp, #32
- .cfi_def_cfa_offset 32
- stp x29, x30, [sp, #16] // 16-byte Folded Spill
- add x29, sp, #16
- .cfi_def_cfa w29, 16
- .cfi_offset w30, -8
- .cfi_offset w29, -16
- mov w8, wzr
- str w8, [sp, #8] // 4-byte Folded Spill
- stur wzr, [x29, #-4]
- adrp x0, .L.str
- add x0, x0, :lo12:.L.str
- bl printf
- ldr w0, [sp, #8] // 4-byte Folded Reload
- .cfi_def_cfa wsp, 32
- ldp x29, x30, [sp, #16] // 16-byte Folded Reload
- add sp, sp, #32
- .cfi_def_cfa_offset 0
- .cfi_restore w30
- .cfi_restore w29
- ret
-.Lfunc_end0:
- .size main, .Lfunc_end0-main
- .cfi_endproc
- // -- End function
- .type .L.str,@object // @.str
- .section .rodata.str1.1,"aMS",@progbits,1
-.L.str:
- .asciz "Hello, Holberton"
- .size .L.str, 17
+section .data
+    msg db 'Hello, Holberton',0xa  ; the string to be printed, terminated with newline (0xa)
 
- .ident "clang version 15.0.7"
- .section ".note.GNU-stack","",@progbits
+section .text
+    global _start
+
+_start:
+    ; write the message to stdout
+    mov eax, 4       ; system call number for write
+    mov ebx, 1       ; file descriptor for stdout
+    mov ecx, msg     ; pointer to the message
+    mov edx, 14      ; message length
+    int 0x80         ; call kernel
+
+    ; exit program with status code 0
+    mov eax, 1       ; system call number for exit
+    xor ebx, ebx     ; exit status code 0
+    int 0x80         ; call kernel
