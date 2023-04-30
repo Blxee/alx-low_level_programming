319e876f920e0bd9e6c5fb38a62a326d97a8ba8c
Blxee <aminedev57@gmail.com>
2023-04-30 14:11:48 +0100

    Fixed: 10-delete_nodeint.c task

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	int value;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	for (i = 0; i < index; i++)
