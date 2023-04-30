72e0ca06064f4e7f6edfb7446085e557e93c67a3
Blxee <aminedev57@gmail.com>
2023-04-30 11:38:57 +0100

    Added: 6-pop_listint.c task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,6 @@ size_t print_listint(const listint_t *h)
 {
 	size_t len;
 
-	if (h == NULL)
-	{
-		printf("\n");
-		return (0);
-	}
-
 	len = 0;
 	while (h != NULL)
 	{
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - removes the last element from a list
+ *
+ * @head: head (first node) of the list
+ *
+ * Return: the removed element
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t **tail;
+	int value;
+
+	if (*tail == NULL)
+	  return (0);
+
+	tail = head;
+	while ((*tail)->next != NULL)
+	  tail = &(*tail)->next;
+
+  value = (*tail)->n;
+	free(*tail);
+
+	return (value);
+}
+
