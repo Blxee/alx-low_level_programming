7132671d9593767d1d79a9c13c81c689ff9d9e8b
Blxee <aminedev57@gmail.com>
2023-05-02 19:58:36 +0100

    Added: 102-free_listint_safe.c task

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include "4-free_listint.c"
+
+/**
+ * free_listint_safe - frees a list from memory (safe version)
+ *
+ * @h: head (first node) of the list
+ *
+ * Return: nodes freed from memory
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len, i;
+	listint_t **buffer, *head = *h, *next;
+
+	buffer = malloc(1024 * sizeof(listint_t *));
+	if (buffer == NULL)
+		return (98);
+
+	len = 0;
+	while (head)
+	{
+		for (i = 0; i < len; i++)
+			if (buffer[i] == head)
+			{
+				buffer = NULL;
+				break;
+			}
+		if (!buffer)
+			break;
+		buffer[len++] = head;
+		next = head->next;
+		free(head);
+		head = next;
+	}
+
+	*h = NULL;
+	free(buffer);
+	return (len);
+}
