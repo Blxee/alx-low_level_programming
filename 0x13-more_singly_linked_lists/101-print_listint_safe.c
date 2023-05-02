e7e6e944b457a16a9ffac2876d5fd209d110d276
Blxee <aminedev57@gmail.com>
2023-05-02 12:09:47 +0100

    Added: 101-print_listint_safe.c task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - prints all elements in a list
+ *
+ * @head: first node of the list
+ *
+ * Return:
+ *	number of nodes
+ *	98 if it fails
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t len, i;
+	const listint_t **buffer;
+
+	buffer = malloc(1024 * sizeof(listint_t *));
+	if (buffer == NULL)
+		return (98);
+
+	len = 0;
+	while (head)
+	{
+		for (i = 0; i < len; i++)
+			if (buffer[i] == head)
+			{
+				buffer = NULL;
+				break;
+			}
+		if (!buffer)
+			break;
+		printf("[%p]%d\n", head, head->n);
+		buffer[len++] = head;
+		head = head->next;
+	}
+
+	free(buffer);
+	return (len);
+}
