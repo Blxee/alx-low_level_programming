e8ae9775d49a703afa4f626b12ab108ca0e6cd8e
Blxee <aminedev57@gmail.com>
2023-05-03 01:18:46 +0100

    Fixed: 102-free_listint_safe.c task

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,13 +13,15 @@ size_t free_listint_safe(listint_t **h)
 	size_t len, i;
 	listint_t **buffer, *head = *h, *next;
 
-	if (!h)
+	if (!h || !*h)
 		return (0);
 
 	buffer = malloc(1024 * sizeof(listint_t *));
+	if (buffer == NULL)
+		return (0);
 
 	len = 0;
-	while (buffer && head)
+	while (head)
 	{
 		for (i = 0; i < len; i++)
 			if (buffer[i] == head)
