18aec6f39521b33c848ee2106ddc04225af1dc71
Blxee <aminedev57@gmail.com>
2023-05-04 22:43:35 +0100

    Fixed: 1-print_binary.c task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,24 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long i, len = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	for (i = 0; i < sizeof(int) * 8; i++)
-		if (n & (1 << i))
-			len = i;
-
-	do {
-		unsigned long bit = 1 << len;
-
-		if (n & bit)
-			_putchar('1');
-		else
-			_putchar('0');
-	} while (len--);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
