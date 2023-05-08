27cbe0591e5f78a6679630cdb38ec1960bc93f90
Blxee <aminedev57@gmail.com>
2023-05-08 13:51:21 +0100

    Fixed: 1-create_file.c task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_no = open(filename, O_WRONLY | O_CREAT, 0600);
+	file_no = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_no == -1)
 		return (-1);
 
