5f29739208a82d770b6601ea88e6ce0ebeb8bf8d
Blxee <aminedev57@gmail.com>
2023-05-08 15:28:48 +0100

    Fixed: 3-cp.c task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,6 +22,30 @@ void try_close(int fd)
 }
 
 /**
+ * open_files - opens the files to copy from and into
+ *
+ * @files: a pointer to strore file descriptors
+ * @f1: name of the 1st file
+ * @f2: name of the 2nd file
+ */
+void open_files(int *files, const char *f1, const char *f2)
+{
+	files[0] = open(f1, O_RDONLY);
+	if (files[0] == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f1);
+		exit(98);
+	}
+	files[1] = open(f2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (files[1] == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f2);
+		try_close(files[0]);
+		exit(99);
+	}
+}
+
+/**
  * main - copies one file into another
  *
  * @argc: argument count
@@ -36,7 +60,7 @@ void try_close(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int f1_no, f2_no;
+	int files[2];
 	char buf[BUF_SIZE];
 	int chars_len;
 
@@ -45,33 +69,23 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	f1_no = open(argv[1], O_RDONLY);
-	if (f1_no == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
-	f2_no = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (f2_no == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		try_close(f1_no);
-		return (99);
-	}
+	open_files(files, argv[1], argv[2]);
 	do {
-		chars_len = read(f1_no, buf, BUF_SIZE);
+		chars_len = read(files[0], buf, BUF_SIZE);
 
 		if (chars_len == -1)
-			goto close_exit;
-		chars_len = write(f2_no, buf, chars_len);
-		if (chars_len == -1)
-			goto close_exit;
-		if (0)
 		{
-close_exit:
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			try_close(f1_no);
-			try_close(f2_no);
+			try_close(files[0]);
+			try_close(files[1]);
+			return (99);
+		}
+		chars_len = write(files[1], buf, chars_len);
+		if (chars_len == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			try_close(files[0]);
+			try_close(files[1]);
 			return (99);
 		}
 	} while (chars_len == BUF_SIZE);
