f435f0756e69a16026e357343b307a0c94e0cf3d
Blxee <aminedev57@gmail.com>
2023-06-15 23:46:02 +0100

    Fixed: 7-insert_dnodeint.c task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,39 +16,56 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
+
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (*h != NULL)
-	{
-		while ((*h)->prev != NULL)
-			h = &(*h)->prev;
-
-		while (i < idx)
-		{
-			if (*h == NULL)
-				return (NULL);
-			h = &(*h)->next;
-			i++;
-		}
-
-		if (*h != NULL)
-		{
-			pre = (*h)->prev;
-			(*h)->prev = NULL;
-		}
-	}
-	else if (idx != 0)
+		return (add_dnodeint_end(h, n));
+	else if (*h == NULL)
 		return (NULL);
+
+	/*
+	while ((*h)->prev != NULL)
+		h = &(*h)->prev;
+	*/
+
+	while (i < idx)
+	{
+		if ((*h)->next == NULL && i == idx - 1)
+			return (add_dnodeint_end(h, n));
+		h = &(*h)->next;
+		i++;
+	}
+
+	pre = (*h)->prev;
+	(*h)->prev = NULL;
 
 	new = add_dnodeint(h, n);
 
-	if (pre)
-	{
-		new->prev = pre;
-		pre->next = new;
-	}
-	else
-		*head = new;
+	new->prev = pre;
+	pre->next = new;
 
 	return (new);
+}
+
+
+
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
 }
