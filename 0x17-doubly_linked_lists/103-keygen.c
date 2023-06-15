74a07730d6b616f6e9194f1729ac198a342594de
Blxee <aminedev57@gmail.com>
2023-06-15 22:42:43 +0100

    Fixed: 103-keygen.c task

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "keygen_tools1.c"
 #include "keygen_tools2.c"
+
+/**
+ * _strlen - retrieves the length of a string
+ *
+ * @str: the string
+ *
+ * Return: length of @str
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
+
+	if (!str)
+		return (len);
+
+	while (str[len])
+		len++;
+	return (len);
+}
 
 /**
  * main - entry point
@@ -14,18 +32,18 @@
  */
 int main(int argc, char *argv[])
 {
-	char *magic_str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *m = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char pwd[7];
 	char *user = argv[1];
-	int len = strlen(user);
+	int len = _strlen(user);
 
 	(void)argc;
-	pwd[0] = magic_str[f1(len)];
-	pwd[1] = magic_str[f2(user, len)];
-	pwd[2] = magic_str[f3(user, len)];
-	pwd[3] = magic_str[f4(user, len)];
-	pwd[4] = magic_str[f5(user, len)];
-	pwd[5] = magic_str[f6(*user)];
+	pwd[0] = m[f1(len)];
+	pwd[1] = m[f2(user, len)];
+	pwd[2] = m[f3(user, len)];
+	pwd[3] = m[f4(user, len)];
+	pwd[4] = m[f5(user, len)];
+	pwd[5] = m[f6(*user)];
 
 	printf("%s", pwd);
 	return (EXIT_SUCCESS);
