92a89c678566ea64cd3de75c0f9997d588bd2ab7
Blxee <aminedev57@gmail.com>
2023-06-15 23:51:05 +0100

    Fixed: 7-insert_dnodeint.c task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,34 +12,42 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new, *pre = NULL;
+	dlistint_t **head = h, *new, *pre = NULL;
 
 	if (h == NULL)
 		return (NULL);
 
-	if (idx == 0)
-		return (add_dnodeint_end(h, n));
-	else if (*h == NULL)
-		return (NULL);
-
-	while ((*h)->prev != NULL)
-		h = &(*h)->prev;
-
-	while (i < idx)
+	if (*h != NULL)
 	{
-		if ((*h)->next == NULL && i == idx - 1)
-			return (add_dnodeint_end(h, n));
-		h = &(*h)->next;
-		i++;
-	}
+		while ((*h)->prev != NULL)
+			h = &(*h)->prev;
 
-	pre = (*h)->prev;
-	(*h)->prev = NULL;
+		while (i < idx)
+		{
+			if (*h == NULL)
+				return (NULL);
+			h = &(*h)->next;
+			i++;
+		}
+
+		if (*h != NULL)
+		{
+			pre = (*h)->prev;
+			(*h)->prev = NULL;
+		}
+	}
+	else if (idx != 0)
+		return (NULL);
 
 	new = add_dnodeint(h, n);
 
-	new->prev = pre;
-	pre->next = new;
+	if (pre)
+	{
+		new->prev = pre;
+		pre->next = new;
+	}
+	else
+		*head = new;
 
 	return (new);
 }
