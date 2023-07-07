d1c5fc0d8fa51412a6c8db9dae2a88592b455134
Blxee <aminedev57@gmail.com>
2023-07-07 11:58:31 +0100

    Fixed: 3-hash_table_set.c task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, **head;
+	hash_node_t *node = NULL, **head = NULL;
 	unsigned long index;
 
 	if (!ht || !key || !*key || !value)
