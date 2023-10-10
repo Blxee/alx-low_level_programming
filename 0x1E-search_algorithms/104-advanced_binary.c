79892264a41a152d2e05c55895df4645687a81bf
Blxee <aminedev57@gmail.com>
2023-10-10 17:57:14 +0100

    Added: 104-advanced_binary.c task

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,52 @@
 #include "search_algos.h"
 
 /**
+ * _advanced_binary - function that searches for a value
+ *	in an array of integers using the Advanced Binary search algorithm
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @left: left index to start searching from
+ * @right: right index to stop searching at
+ * @value: the value to search for
+ *
+ * Return: the first index where value is located
+ */
+int _advanced_binary(int *array, size_t left, size_t right, int value)
+{
+	size_t i, m;
+
+	if (left >= right)
+		return (-1);
+
+	printf("Searching in array: ");
+	i = left;
+	while (i <= right)
+		if (i == right)
+			printf("%d\n", array[i++]);
+		else
+			printf("%d, ", array[i++]);
+
+	m = (left + right) / 2;
+
+	if (value == array[m])
+	{
+		if (m > left && array[m - 1] == value)
+			return (_advanced_binary(array, left, m, value));
+		else
+			return (m);
+	}
+	else if (right - left == 1)
+		return (-1);
+
+	if (value < array[m])
+		return (_advanced_binary(array, left, m - 1, value));
+	else if (value > array[m])
+		return (_advanced_binary(array, m + 1, right, value));
+
+	return (-1);
+}
+
+/**
  * advanced_binary - function that searches for a value
  *	in an array of integers using the Advanced Binary search algorithm
  *
@@ -12,33 +58,7 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t m, l = 0, r = size - 1;
-	size_t i;
-
 	if (array == NULL)
 		return (-1);
-
-	while (l <= r)
-	{
-		printf("Searching in array: ");
-		i = l;
-		while (i <= r)
-			if (i == r)
-				printf("%d\n", array[i++]);
-			else
-				printf("%d, ", array[i++]);
-		m = (l + r) / 2;
-		if (value < array[m])
-			r = m - 1;
-		else if (value > array[m])
-			l = m + 1;
-		else
-		{
-			while (m > 0 && array[m - 1] == array[m])
-				m--;
-			return (m);
-		}
-	}
-
-	return (-1);
+	return (_advanced_binary(array, 0, size - 1, value));
 }
